e3cccd7f067a157bf59bc644a74d190d80b281b8
Luan Phelippe <luan45000@gmail.com>
2024-12-14 11:31:01 -0300

    WIP: rename files from fdcan to app

--- FDSafe_Alice/Core/Inc/main.h
+++ FDSafe_Alice/Core/Inc/main.h
@@ -32,7 +32,7 @@ extern "C" {
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
-#include "fdsafe.h"
+#include <app.h>
 
 /* USER CODE END Includes */
 
--- FDSafe_Alice/Core/Src/app.c
+++ FDSafe_Alice/Core/Src/app.c
@@ -10,7 +10,7 @@
  */
 
 
-#include "fdsafe.h"
+#include <app.h>
 #include "main.h"
 
 
